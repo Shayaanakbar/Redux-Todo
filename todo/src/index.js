e3b5f048d28658463658ed604f0ddde5833d069e
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-13 13:26:32 -0500

    MVP

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {todoReducer} from './Reducers/index';
+import ToDoList from './Components/ToDoList';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(todoReducer);
+
+
+function App() {
+    return (
+        <div className="App">
+            < ToDoList/>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
